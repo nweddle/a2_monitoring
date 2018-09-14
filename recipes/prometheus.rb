854c7a93addc907aa8ad6d944561dc70728c2589
Nathan Weddle <nathan.weddle@alaskaair.com>
2018-09-13 17:51:22 -0700

    Update prometheus version
    Add elasticsearch_exporter configs

--- recipes/prometheus.rb
+++ recipes/prometheus.rb
@@ -1,43 +1,34 @@
-remote_file '/tmp/prometheus-2.2.1.linux-amd64.tar.gz' do
-  source 'https://github.com/prometheus/prometheus/releases/download/v2.2.1/prometheus-2.2.1.linux-amd64.tar.gz'
+remote_file '/tmp/prometheus-2.4.0.linux-amd64.tar.gz' do
+  source 'https://github.com/prometheus/prometheus/releases/download/v2.4.0/prometheus-2.4.0.linux-amd64.tar.gz'
 end
 
-execute 'tar xvf /tmp/prometheus-2.2.1.linux-amd64.tar.gz' do
+execute 'tar xvf /tmp/prometheus-2.4.0.linux-amd64.tar.gz' do
   cwd '/opt/'
 end
 
 link '/opt/prometheus' do
-  to '/opt/prometheus-2.2.1.linux-amd64'
+  to '/opt/prometheus-2.4.0.linux-amd64'
 end
 
 file '/opt/prometheus/prometheus.yml' do
   content <<EOF
 global:
-  scrape_interval:     15s # By default, scrape targets every 15 seconds.
-
-  # Attach these labels to any time series or alerts when communicating with
-  # external systems (federation, remote storage, Alertmanager).
+  scrape_interval: 15s
   external_labels:
     monitor: 'codelab-monitor'
-
-# A scrape configuration containing exactly one endpoint to scrape:
-# Here it's Prometheus itself.
 scrape_configs:
-  # The job name is added as a label `job=<job_name>` to any timeseries scraped from this config.
   - job_name: 'prometheus'
-
-    # Override the global default and scrape targets from this job every 5 seconds.
     scrape_interval: 5s
-
     static_configs:
       - targets: ['localhost:9090']
   - job_name: 'node'
-
-    # Override the global default and scrape targets from this job every 5 seconds.
     scrape_interval: 15s
-
     static_configs:
       - targets: ['localhost:9100']
+  - job_name: 'es'
+    scrape_interval: 15s
+    static_configs:
+      - targets: ['localhost:9108']
 EOF
 end
 
